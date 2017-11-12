32f30b725ad30def1b3cd3dad572b83644feaad9
Vinny Cerchia <vcerchia@commercehub.com>
2017-11-11 22:10:14 -0500

    Add eslint

--- test/S3Store.spec.js
+++ test/S3Store.spec.js
@@ -1,21 +1,20 @@
-const expect = require('chai').expect;
 const standardTests = require('passwordless-tokenstore-test');
 const S3Store = require('../lib/S3Store');
 
 const testBucket = '';
 if (!testBucket) {
-	throw new Error('A test bucket must be specified.');
+    throw new Error('A test bucket must be specified.');
 }
 function TokenStoreFactory() {
-	return new S3Store({params: {Bucket: testBucket}});
+    return new S3Store({params: {Bucket: testBucket}});
 }
 
 const beforeEachTest = function (done) {
-	done();
-}
+    done();
+};
 
 const afterEachTest = function (done) {
-	done();
-}
+    done();
+};
 
 standardTests(TokenStoreFactory, beforeEachTest, afterEachTest, 1000);
