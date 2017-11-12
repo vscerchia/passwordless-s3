3be3cf9713fbc59ad1e4863fa5d2a9c5aa822451
Vinny Cerchia <vcerchia@commercehub.com>
2017-11-11 22:04:21 -0500

    cleanup test

--- test/S3Store.spec.js
+++ test/S3Store.spec.js
@@ -2,7 +2,10 @@ const expect = require('chai').expect;
 const standardTests = require('passwordless-tokenstore-test');
 const S3Store = require('../lib/S3Store');
 
-const testBucket = 'FILL IN YOUR BUCKET';
+const testBucket = '';
+if (!testBucket) {
+	throw new Error('A test bucket must be specified.');
+}
 function TokenStoreFactory() {
 	return new S3Store({params: {Bucket: testBucket}});
 }
@@ -16,14 +19,3 @@ const afterEachTest = function (done) {
 }
 
 standardTests(TokenStoreFactory, beforeEachTest, afterEachTest, 1000);
-
-describe('Your specific tests', function () {
-
-	beforeEach((done) => {
-        beforeEachTest(done);
-	});
-
-	afterEach(function(done) {
-		afterEachTest(done);
-	});
-});
