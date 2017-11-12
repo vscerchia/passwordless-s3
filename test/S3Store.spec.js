3eff09a84e23343f608f60da29f882cabd900daa
Vinny Cerchia <vcerchia@commercehub.com>
2017-11-11 21:40:38 -0500

    Add index.js

--- test/S3Store.spec.js
+++ test/S3Store.spec.js
@@ -2,7 +2,7 @@ const expect = require('chai').expect;
 const standardTests = require('passwordless-tokenstore-test');
 const S3Store = require('../lib/S3Store');
 
-const testBucket = 'commercehub-pstr-vinny-passwordless';
+const testBucket = 'FILL IN YOUR BUCKET';
 function TokenStoreFactory() {
 	return new S3Store({params: {Bucket: testBucket}});
 }
